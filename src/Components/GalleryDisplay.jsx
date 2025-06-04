47005dc1eac78af86524f9eec5dea9f80ce3abd8
b1endong <tranhoanghai260805@gmail.com>
2025-06-05 02:45:55 +0700

    something

--- src/Components/GalleryDisplay.jsx
+++ src/Components/GalleryDisplay.jsx
@@ -1,33 +1,47 @@
-import React from 'react'
-import hoodie from '../../public/Assets/Hoodies.jpeg'
-import tshirt from '../../public/Assets/T-Shirt.jpeg'
-import pants from '../../public/Assets/Pants.jpeg'
-import shirts from '../../public/Assets/Shirts.jpeg'
-import {Link} from 'react-router-dom'
+import React from "react";
+import {Link} from "react-router-dom";
+
+// Trong Vite, assets trong public/ phải tham chiếu bằng đường dẫn tuyệt đối
+const hoodie = "/Assets/Hoodies.jpeg";
+const tshirt = "/Assets/T-Shirt.jpeg";
+const pants = "/Assets/Pants.jpeg";
+const shirts = "/Assets/Shirts.jpeg";
 
 const GalleryDisplay = () => {
-  return (
-    <div className='galleryDisplay'>
-        <div className='heading'>OUR CATEGORIES</div>
-        <div className='galleryItemList'>
-                <div className='galleryItem bg-cover bg-[20%_30%]' style={{backgroundImage: `url(${shirts})`}}>
-                    <div className='itemTitle'>Shirts</div>
+    return (
+        <div className="galleryDisplay">
+            <div className="heading">OUR CATEGORIES</div>
+            <div className="galleryItemList">
+                <div
+                    className="galleryItem bg-cover bg-[20%_30%]"
+                    style={{backgroundImage: `url(${shirts})`}}
+                >
+                    <div className="itemTitle">Shirts</div>
                 </div>
 
-                <div className='galleryItem col-span-2 bg-cover bg-[20%_60%]' style={{backgroundImage: `url(${pants})`}}>
-                    <div className='itemTitle'>Pants</div>
+                <div
+                    className="galleryItem col-span-2 bg-cover bg-[20%_60%]"
+                    style={{backgroundImage: `url(${pants})`}}
+                >
+                    <div className="itemTitle">Pants</div>
                 </div>
 
-                <div className="galleryItem col-span-2 bg-cover bg-[20%_30%]" style={{backgroundImage: `url(${hoodie})`}}>
-                    <div className='itemTitle absolute'>Hoodies</div>
+                <div
+                    className="galleryItem col-span-2 bg-cover bg-[20%_30%]"
+                    style={{backgroundImage: `url(${hoodie})`}}
+                >
+                    <div className="itemTitle absolute">Hoodies</div>
                 </div>
 
-                <div className='galleryItem bg-cover bg-[20%_40%]' style={{backgroundImage: `url(${tshirt})`}}>
-                    <div className='itemTitle'>T-Shirts</div>
+                <div
+                    className="galleryItem bg-cover bg-[20%_40%]"
+                    style={{backgroundImage: `url(${tshirt})`}}
+                >
+                    <div className="itemTitle">T-Shirts</div>
                 </div>
+            </div>
         </div>
-    </div>
-  )
-}
+    );
+};
 
-export default GalleryDisplay
+export default GalleryDisplay;
